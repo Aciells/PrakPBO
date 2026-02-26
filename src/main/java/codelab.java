b03150053cb883cb675620c58f33ebf69d403734
cod-java <marcelloairlangga18@gmail.com>
2026-02-26 22:46:59 +0800

    Initial commit

---
+++ src/main/java/codelab.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class codelab {
+    static void main() {
+                Scanner choose = new Scanner(System.in);
+                String pilihan;
+
+                System.out.println("Welcome to Smart City Management System");
+                System.out.println("1");
+                System.out.println("2");
+                System.out.println("pilih : ");
+                pilihan = choose.nextLine();
+
+                System.out.println("pilihan :" + pilihan);
+
+    }
+}
